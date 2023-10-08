8ee22b61551678b8d2d8c23f8676e159eb5318e7
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-10-08 17:22:54 +0900

    feat: 라이브러리 설치

--- udem/이정환-react&next/section1/src/index.js
+++ udem/이정환-react&next/section1/src/index.js
@@ -1,5 +1,5 @@
 import mul, {add, sub} from "./math.js"
+import randomColor from "randomcolor"
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(10, 20));
+const color = randomColor()
+console.log(color)

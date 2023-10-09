6162ebd785f7c882cb58c687fff6f3d6dbcb404a
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-10-09 14:19:48 +0900

    feat: section3 완료

--- udem/이정환-react&next/section2-3/src/components/Body.jsx
+++ udem/이정환-react&next/section2-3/src/components/Body.jsx
@@ -1,7 +1,62 @@
+import "./Body.css";
+import { useState, useRef } from "react";
+
 export default function Body() {
-    return (
-        <div>
-            <h1>BODY</h1>
-        </div>
-    )
+  const nameRef = useRef();
+  const [state, setState] = useState({
+    name: "",
+    gender: "",
+    bio: "",
+  });
+
+  const onChange = (e) => {
+    console.log(e.target.name + " : " + e.target.value);
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = () => {
+    if (state.name === "") {
+      nameRef.current.focus();
+      return;
+    }
+    alert(`${state.name}님 회원가입을 축하합니다!`);
+  };
+
+  return (
+    <div className="body">
+      <div>
+        <input
+          ref={nameRef}
+          name="name"
+          placeholder="이름"
+          value={state.name}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <select
+          name="gender"
+          value={state.gender}
+          onChange={onChange}
+        >
+          <option value="">밝히지 않음</option>
+          <option value="male">남성</option>
+          <option value="female">여성</option>
+        </select>
+      </div>
+      <div>
+        <textarea
+          name="bio"
+          value={state.bio}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <button onClick={onSubmit}>회원가입</button>
+      </div>
+    </div>
+  );
 }

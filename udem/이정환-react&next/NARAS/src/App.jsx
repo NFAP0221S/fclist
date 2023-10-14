c1f490b16b489a5285f464b47374e9f1ad323c1c
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-10-15 00:00:19 +0900

    app 컴포넌트에 페이지 설정

--- udem/이정환-react&next/NARAS/src/App.jsx
+++ udem/이정환-react&next/NARAS/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Search from './pages/Search'
+import Country from './pages/Country'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/search' element={<Search/>} />
+      <Route path='/country' element={<Country/>} />
+    </Routes>
   )
 }
 

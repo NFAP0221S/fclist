5fbd67f87ae654d42c80b449b8e2b23d1bacac73
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-10-19 01:58:21 +0900

    all 데이터 api요청

--- udem/이정환-react&next/NARAS/src/pages/Home.jsx
+++ udem/이정환-react&next/NARAS/src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
+import { fetchCountries } from "../api";
+
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+  const [countries, setCountries] = useState();
+
+  const setInitData = async () => {
+    const data = await fetchCountries();
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    setInitData();
+  }, []);
+
+  return <div>Home</div>;
 }

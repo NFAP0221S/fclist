36886172ac6edce48f5b9ff7a23314a890ac1693
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-10-23 23:34:54 +0900

    구조, 라우터 설정

---
+++ fc/10-pj-blog-app/src/components/Router.tsx
@@ -0,0 +1,27 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "../pages/home";
+import PostsList from "../pages/posts";
+import Detail from "../pages/posts/detail";
+import Edit from "../pages/posts/edit";
+import New from "../pages/posts/new";
+import Profile from "../pages/profile";
+import Login from "../pages/login";
+import Signup from "../pages/signup";
+
+export default function Router() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostsList />} />
+        <Route path="/posts/:id" element={<Detail />} />
+        <Route path="/posts/new" element={<New />} />
+        <Route path="/posts/edit/:id" element={<Edit />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Navigate replace to={"/"} />} />
+      </Routes>
+    </>
+  );
+}
---
+++ fc/10-pj-blog-app/src/pages/posts/new.tsx
@@ -0,0 +1,3 @@
+export default function New() {
+  return <h1>Post New Page</h1>;
+}

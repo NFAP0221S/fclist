c09cd742c90c606129bd5704c25ba8d4a6193b7f
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-10-29 21:46:46 +0900

    NavLink 사용

--- udem/max-react/sec21-spa-다중페이지-구축/src/components/MainNavigation.js
+++ udem/max-react/sec21-spa-다중페이지-구축/src/components/MainNavigation.js
@@ -1,22 +1,37 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css'
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
   return (
     <header className={classes.header}>
-        <nav className={classes.nav}>
-            <ul className={classes.list}>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/products'>products</Link>
-                </li>
-            </ul>
-        </nav>
+      <nav className={classes.nav}>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
 
 export default MainNavigation;

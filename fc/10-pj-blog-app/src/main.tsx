b36c6a7f05eff663f4ec3cd4e085b03679f0fdac
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-10-30 23:45:02 +0900

    vite에 firebase 셋팅하기

--- fc/10-pj-blog-app/src/main.tsx
+++ fc/10-pj-blog-app/src/main.tsx
@@ -4,6 +4,9 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import firebase from "firebaseApp";
+
+console.log(firebase);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Router>
     <App />

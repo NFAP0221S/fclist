2c785e196c93cf9fd9eaca03c78fc6de9263317f
Hansol <102638663+NFAP0221S@users.noreply.github.com>
2023-12-16 18:30:03 +0900

    app 라우팅 기본 셋팅

---
+++ inf/zerocho/next14-x-clone/src/app/(afterLogin)/explore/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function ExplorePage() {
+  return <div>explore page</div>;
+}
---
+++ inf/zerocho/next14-x-clone/src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function HomePage() {
+  return <main>홈페이지</main>;
+}
